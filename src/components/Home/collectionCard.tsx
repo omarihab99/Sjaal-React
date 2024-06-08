7f898327d2e4323e837053a4369dc76d4d4ac296
omarihab99 <omar.ihab.mohamed99@gmail.com>
2024-06-08 19:40:26 +0300

    Added collectioncard component

--- src/components/Home/collectionCard.tsx
+++ src/components/Home/collectionCard.tsx
@@ -1,11 +1,54 @@
-import React from 'react';
-
-const CollectionCard = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+import React from "react";
+import { ICollection } from "../../models/ICollection";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowRight,
+  faArrowRightLong,
+} from "@fortawesome/free-solid-svg-icons";
+import classNames from "classnames";
+import collectionCardStyles from "../../styles/collectioncard.module.css";
+const CollectionCard = ({ collection }: { collection: ICollection }) => {
+  const collectionContainerClasses = classNames(
+    collectionCardStyles.collectionContainer,
+    "m-3"
+  );
+  const imgContainerClasses = classNames(
+    collectionCardStyles.imgContainer,
+    "col-12",
+    "p-0"
+  );
+  const [arrowType, setArrowType] = React.useState(faArrowRight);
+  const onCollectionClick = () => {
+    localStorage.setItem("collectionId", collection.id);
+  };
+  const onCollectionMouseOver = () => {
+    setArrowType(faArrowRightLong);
+  };
+  const onCollectionMouseOut = () => {
+    setArrowType(faArrowRight);
+  };
+  return (
+    // <a>
+    <>
+      <div
+        className={collectionContainerClasses}
+        onMouseOver={onCollectionMouseOver}
+        onMouseOut={onCollectionMouseOut}
+      >
+        <Link to={`/collections/${collection.id}`} onClick={onCollectionClick}>
+          <div className={imgContainerClasses}>
+            <img src={`images/${collection.imgName}`} alt={collection.name} />
+          </div>
+        </Link>
+        <Link to={`collections/${collection.name.split(" ").join("-")}`}>
+          {collection.name}
+          <FontAwesomeIcon icon={arrowType} />
+        </Link>
+      </div>
+    </>
+    // </a>
+  );
+};
 
 export default CollectionCard;
