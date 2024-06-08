42b932bd3f0be6b2ec1e50aeb0915995568b19b6
omarihab99 <omar.ihab.mohamed99@gmail.com>
2024-06-08 22:20:03 +0300

    Added sidebar to homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,6 +3,7 @@ import MainSection from '../components/Home/MainSection';
 import ShowCategories from '../components/Home/showCategories';
 import { useAppDispatch , useAppSelector } from '../hooks/useTypedReduxHooks';
 import { fetchCategories } from '../redux/slices/categorySlice';
+import Sidebar from '../components/sidebar';
 const HomePage = () => {
     const dispatch = useAppDispatch();
     const categories = useAppSelector(state => state.categories.categories);
@@ -11,6 +12,7 @@ const HomePage = () => {
     },[]);
     return (
         <div>
+            {/* <Sidebar /> */}
             <MainSection />
             <ShowCategories />
         </div>
