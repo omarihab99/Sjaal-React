6502fdccf15e71d9b33d633026664154aac84f18
omarihab99 <omar.ihab.mohamed99@gmail.com>
2024-06-08 19:39:06 +0300

    Added home page with its components

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import MainSection from '../components/Home/MainSection';
+import ShowCategories from '../components/Home/showCategories';
+import { useAppDispatch , useAppSelector } from '../hooks/useTypedReduxHooks';
+import { fetchCategories } from '../redux/slices/categorySlice';
 const HomePage = () => {
+    const dispatch = useAppDispatch();
+    const categories = useAppSelector(state => state.categories.categories);
+    useEffect(() => {
+        dispatch(fetchCategories());
+    },[]);
     return (
         <div>
-            
+            <MainSection />
+            <ShowCategories />
         </div>
     );
 }
