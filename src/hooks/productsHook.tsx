b1fb9d4ad996cfa3c9488845e93a27fcb72554ce
omarihab99 <omar.ihab.mohamed99@gmail.com>
2024-06-08 19:37:54 +0300

    updated typed hooks file name

--- src/hooks/productsHook.tsx
+++
@@ -1,6 +0,0 @@
-import { useDispatch, useSelector } from "react-redux";
-import { AppDispatch, RootState } from "../redux/store";
-
-
-export const productsDispatch =useDispatch.withTypes<AppDispatch>();
-export const productsSelector =useSelector.withTypes<RootState>();
